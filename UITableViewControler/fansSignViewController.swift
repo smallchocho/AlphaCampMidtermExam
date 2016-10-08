0cd565f84f39bd43358bb000db200fe9282e5973
smallchocho <bigchocho@gmail.com>
2016-10-08 13:48:21 +0800

    fifth commit

--- UITableViewControler/fansSignViewController.swift
+++ UITableViewControler/fansSignViewController.swift
@@ -10,31 +10,40 @@ import UIKit
 
 class fansSignViewController: UIViewController {
     
+
     @IBOutlet weak var fansSighTextField: UITextField!
-    @IBOutlet weak var fansSignButton: UIButton!
-    
-    @IBOutlet weak var fansSignLabel: UILabel!
+    @IBOutlet weak var fansSighTextView: UITextView!
     @IBOutlet weak var errorNotice: UILabel!
+    var signCount = 0
+    @IBAction func updateSign(_ sender: UIButton) {
+        if var fansSign = fansSighTextField.text,fansSighTextField.text != "" {
+            if signCount == 0{
+                errorNotice.text = ""
+                fansSighTextView.text = fansSign + "\n"
+                fansSighTextField.text = ""
+                signCount = signCount + 1
+                print("signCount:\(signCount)")
+            }else if signCount < 3{
+                errorNotice.text = ""
+                fansSign = fansSign + "\n"
+                fansSighTextView.text = fansSighTextView.text! + fansSign
+                fansSighTextField.text = ""
+                signCount = signCount + 1
+                print("signCount:\(signCount)")
+            }else if signCount >= 3{
+                errorNotice.text = "不可輸入超過三次"
+            }
+
+        }else{
+            errorNotice.text = "不可輸入空白或不輸入"
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
